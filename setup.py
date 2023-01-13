b49cda22fb09e2967325a3d518825dec612ae4e7
Madeleine Harbom <madeleine.harbom@gmail.com>
2023-01-13 13:31:32 +0100

    setup.py added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='python-architecture',
+    version='',
+    packages=['tests', 'domain', 'examples', 'examples.encapsulation', 'examples.value_objects'],
+    url='',
+    license='',
+    author='madeleineharbom',
+    author_email='',
+    description=''
+)
